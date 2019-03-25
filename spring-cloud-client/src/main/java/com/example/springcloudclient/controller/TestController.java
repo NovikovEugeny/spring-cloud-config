ca72402f5de3bccdca7df61ebc25868fbeea003b
EugenyNovikov <zhenyanovikov96@gmail.com>
2019-03-26 00:03:53 +0300

    first commit

---
+++ spring-cloud-client/src/main/java/com/example/springcloudclient/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.springcloudclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${info.text}")
+    private String message;
+
+    @GetMapping("/messages")
+    public String getMessage() {
+        return message;
+    }
+
+}
